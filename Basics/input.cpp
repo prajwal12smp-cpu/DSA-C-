a976deab69b3a9c59b0086746e6a163b89f80cc0
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-02-28 16:22:42 +0530

    add changes

--- Basics/input.cpp
+++ Basics/input.cpp
@@ -1,3 +1,5 @@
+// This program demonstrates how to take input from the user in C++ using the cin object. It prompts the user to enter their age and then displays the entered age back to the user.
+
 #include<iostream>
 using namespace std;
 int main(){
