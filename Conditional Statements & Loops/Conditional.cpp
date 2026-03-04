4928842f4f42e7900963d19b9842ffad67c88a78
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-03-04 16:01:25 +0530

    updated conditional

--- Conditional Statements & Loops/Conditional.cpp
+++ Conditional Statements & Loops/Conditional.cpp
@@ -11,7 +11,6 @@
 // if-else statement
 #include <iostream>
 using namespace std;
-
 int main() {
     int n = -45;
 
@@ -20,8 +19,21 @@ int main() {
     } else {
         cout << "n is a negative number\n";
     }
-
     return 0;
 }
 
+// if-else statement with user input
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age >= 18) {
+        cout << "You are an adult and you can vote.\n";
+    } else {
+        cout << "You are a minor and you cannot vote.\n";
+    }
+    return 0;
+}
     
