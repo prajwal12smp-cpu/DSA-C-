48a0c2dfb45ab8e530042e731f3123c1c2c4d3cc
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-03-04 15:48:16 +0530

    added if else statement

--- Conditional Statements & Loops/Conditional.cpp
+++ Conditional Statements & Loops/Conditional.cpp
@@ -1,3 +1,27 @@
 // conditional statements
+// conditional statements are used to perform different actions based on different conditions.
+// C++ supports the following conditional statements:
+// 1. if statement
+// 2. if...else statement
+// 3. if...else if...else statement
+// 4. switch statement
+// 5. ternary operator
 
+
+// if-else statement
 #include <iostream>
+using namespace std;
+
+int main() {
+    int n = -45;
+
+    if (n>=0) {
+        cout << "n is a positive number\n";
+    } else {
+        cout << "n is a negative number\n";
+    }
+
+    return 0;
+}
+
+    
