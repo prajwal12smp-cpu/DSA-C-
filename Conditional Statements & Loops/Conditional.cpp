36a75440bd7237e568ff53e678432c46fac05320
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-03-04 16:42:25 +0530

    add changes

--- Conditional Statements & Loops/Conditional.cpp
+++ Conditional Statements & Loops/Conditional.cpp
@@ -36,4 +36,19 @@ int main() {
     }
     return 0;
 }
-    
+
+// if-else statement to check if a number is even or odd
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n%2 == 0) {
+        cout << "The number is even.\n";
+    } else {
+        cout << "The number is odd.\n";
+    }
+    return 0;
+
+}
